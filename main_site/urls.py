6bfa6f98add97f436048dafc24b057fddb6cb2cb
ryuga <vaishnav2353@gmail.com>
2020-11-30 00:56:44 +0530

    Added links

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from . import views
+from .views import DynamicView, ContactView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^pages/privacy/', views.DynamicPageView.as_view(), name='projects'),
+
+    path('', DynamicView.as_view(), name='home'),
+    url(r'^pages/terms/', DynamicView.as_view(template_name="terms-condition.html"), name='terms'),
+    url(r'^pages/about/', DynamicView.as_view(template_name="about.html"), name='about'),
+    url(r'^pages/privacy/', DynamicView.as_view(template_name="privacy-policy.html"), name='privacy'),
+    url(r'^pages/discord-hosting/', DynamicView.as_view(template_name="discord-hosting.html"), name='discord'),
+    url(r'^pages/contact/', ContactView.as_view(), name='contact'),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -4,26 +4,15 @@ from django.views import View
 # Create your views here.
 
 
-def index(request):
-    return render(request, "index.html")
+class DynamicView(View):
+    template_name = "index.html"
 
-
-def about(request):
-    return render(request, "about.html")
-
-
-def privacy(request):
-    return render(request, "privacy-policy.html")
-
-
-def terms(request):
-    return render(request, "terms-condition.html")
+    def get(self, request):
+        return render(request, self.template_name)
 
 
 class ContactView(View):
+    template_name = "contact.html"
 
-    def get(self, request, name=None):
-        if name is None:
-            return render(request, "index.html")
-        else:
-            return render(request, f"{name}.html")
+    def get(self, request):
+        return render(request, self.template_name)
