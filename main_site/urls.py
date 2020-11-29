636712d2006adf1fa4472e99adffd76eda71b6ff
ryuga <vaishnav2353@gmail.com>
2020-11-30 00:23:41 +0530

    Page configuration

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+
 urlpatterns = [
-    path('x', views.index),
+    url(r'^pages/privacy/', views.DynamicPageView.as_view(), name='projects'),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
 
 def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
     return render(request, "about.html")
+
+
+def privacy(request):
+    return render(request, "privacy-policy.html")
+
+
+def terms(request):
+    return render(request, "terms-condition.html")
+
+
+class ContactView(View):
+
+    def get(self, request, name=None):
+        if name is None:
+            return render(request, "index.html")
+        else:
+            return render(request, f"{name}.html")
