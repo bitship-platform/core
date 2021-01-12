94eccb2e8fbe4cda1ac330f03645ce2d33296a3b
ryuga <vaishnav2353@gmail.com>
2021-01-12 20:49:20 +0530

    Edit urls

--- nova_dash/urls.py
+++ nova_dash/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-from .views import DashView, BillingView, ProfileView, LoginView, LogoutView, ManageView, SettingView, media_access
+from .views import DashView, BillingView, LoginView, LogoutView, ManageView, SettingView, media_access
 from django.conf.urls import url
 urlpatterns = [
 
     path('', LoginView.as_view(), name='home'),
     path('login/', LoginView.as_view()),
     url(r'^panel/', DashView.as_view(), name="panel"),
-    url(r'^profile/', ProfileView.as_view(), name='profile'),
+    url(r'^billing/', BillingView.as_view(), name='billing'),
     url(r'^manage/(?P<app_id>[0-9]+)/(?P<folder_id>.+)', ManageView.as_view(), name='browse'),
     url(r'^manage/(?P<app_id>[0-9]+)', ManageView.as_view(), name='manage'),
     url(r'^manage/', ManageView.as_view(), name='rename'),
-    url(r'^billing/', BillingView.as_view(), name='billing'),
     url(r'^settings/', SettingView.as_view(), name='settings'),
     url(r'^logout/', LogoutView.as_view(), name='logout'),
     url(r'^accounts/login/', LoginView.as_view(), name='login'),
