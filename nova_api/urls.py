a77f29fbbffc48f10eb7d3e813498b80b0c2541d
ryuga <vaishnav2353@gmail.com>
2021-01-28 20:15:28 +0530

    App subscription renewal

--- nova_api/urls.py
+++ nova_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls import url
-from .views import AddItemView
+from .views import RenewSubscription
 from django.conf.urls.static import static
+
 urlpatterns = [
 
-    path('data/manage/', AddItemView.as_view(), name='home'),
+    path('subscription/renew/', RenewSubscription.as_view(), name='home'),
 ]
--- nova_api/views.py
+++ nova_api/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import JsonResponse
+from utils.mixins import ResponseMixin
+from nova_dash.models import App
 # Create your views here.
 
 
-class AddItemView(APIView):
+class RenewSubscription(APIView, ResponseMixin):
+
     def post(self, request):
-        print("API REQUEST")
-        print(request.user)
-        return JsonResponse(data={"test": "ok"}, status=200)
+        app_id = request.data.get("app_id")
+        if app_id:
+            try:
+                app = App.objects.get(unique_id=app_id)
+                if app.owner.credits >= app.plan:
+                    app.owner.credits -= app.plan
+                    app.owner.credits_spend += app.plan
+                    app.owner.save()
+                    return self.json_response_200()
+                else:
+                    # TODO: Notify the user by email and discord
+                    pass
+            except App.DoesNotExist:
+                return self.json_response_404()
+
