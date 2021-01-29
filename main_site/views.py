ffc18f8ba24ba639e4cd064600748dbe80cc14ed
ryuga <vaishnav2353@gmail.com>
2021-01-29 21:01:10 +0530

    prevent bot action on contact submission

--- main_site/views.py
+++ main_site/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import View
 from utils.handlers import hook
-from django.conf import settings
 
 
 class DynamicView(View):
@@ -13,20 +12,21 @@ class DynamicView(View):
 
 class ContactView(View):
     template_name = "contact.html"
-    params = ('name', 'email', 'discord', 'msg_subject', 'message')
+    params = ('name', 'email', 'discord', 'msg_subject', 'message', "accepttc")
 
     def get(self, request):
         return render(request, self.template_name)
 
     def post(self, request):
-        data = "".join(f"**{param}**: `{request.POST[param]}`\n"
-                       for param in request.POST if param != "csrfmiddlewaretoken")
-        embed = {"title": "Query submission",
-                 "description": data,
-                 "color": 0xFF7900,
-                 "footer": {
-                     "text": "powered by Novanodes"
-                 }
-                 }
-        hook.send_embed(embed)
+        if request.POST.get("accepttc"):
+            data = "".join(f"**{param}**: `{request.POST[param]}`\n"
+                           for param in request.POST if param != "csrfmiddlewaretoken")
+            embed = {"title": "Query submission",
+                     "description": data,
+                     "color": 0xFF7900,
+                     "footer": {
+                         "text": "powered by Novanodes"
+                     }
+                     }
+            hook.send_embed(embed)
         return render(request, self.template_name)
