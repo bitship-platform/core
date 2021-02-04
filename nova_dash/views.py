6dc936eb9c7c107b9edab519aab51a6ab2862e7f
ryuga <vaishnav2353@gmail.com>
2021-02-04 23:58:35 +0530

    minor changes

--- nova_dash/views.py
+++ nova_dash/views.py
@@ -77,14 +77,14 @@ class BackupDownload(View, ResponseMixin):
         if request.user.is_authenticated:
             try:
                 app = App.objects.get(unique_id=uu_id)
+                if app.owner == request.user.customer:
+                    response = HttpResponse()
+                    del response['Content-Type']
+                    response['X-Accel-Redirect'] = "/protected/media/" + f"{app.unique_id}.tar.gz"
+                    response['Content-Disposition'] = f"attachment; filename={app.unique_id}.tar.gz"
+                    return response
             except App.DoesNotExist:
                 return self.json_response_404()
-            if app.owner == request.user.customer:
-                response = HttpResponse()
-                del response['Content-Type']
-                response['X-Accel-Redirect'] = "/protected/media/" + f"{app.unique_id}.tar.gz"
-                response['Content-Disposition'] = f"attachment; filename={app.unique_id}.tar.gz"
-                return response
         return HttpResponseForbidden('Not authorized to access this file.')
 
 
