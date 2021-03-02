759b64a37041be9b78e05b49c389c54e4c8f38b1
ryuga <vaishnav2353@gmail.com>
2021-03-02 12:41:54 +0530

    urls update

--- nova_dash/urls.py
+++ nova_dash/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, re_path, include
 from .views import DashView, BillingView, LoginView, LogoutView, ManageView, SettingView, media_access,\
-    Transaction, TarballDownload, AppManageView, set_app_config, BackupDownload, ActivityView, TransactionView
+    PaypalTransaction, TarballDownload, AppManageView, set_app_config, BackupDownload, ActivityView, TransactionView
 from django.conf.urls import url
 urlpatterns = [
 
     path('', LoginView.as_view(), name='home'),
     path('login/', LoginView.as_view()),
     url(r'^panel/', DashView.as_view(), name="panel"),
-    url(r'^billing/process/', Transaction.as_view(), name='billing_process'),
+    url(r'^billing/process/', PaypalTransaction.as_view(), name='billing_process'),
     url(r'^billing/', BillingView.as_view(), name='billing'),
     url(r'^manage/(?P<app_id>[0-9]+)/(?P<folder_id>.+)', ManageView.as_view(), name='browse'),
     url(r'^manage/(?P<app_id>[0-9]+)', ManageView.as_view(), name='manage'),
