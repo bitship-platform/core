16921b42cb3cd03cf643071827a99b0495ff197c
ryuga <vaishnav2353@gmail.com>
2021-04-01 14:57:43 +0530

    Some more changes

--- nova_dash/views.py
+++ nova_dash/views.py
@@ -114,7 +114,6 @@ class AdminLoginView(View):
     def post(self, request):
         username = request.POST.get("username")
         password = request.POST.get("password")
-        print(username, password)
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.customer.banned:
