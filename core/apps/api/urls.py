bbeb8e411c2ec7c2477fef1d873c8b5ad9cb547a
ryuga <vaishnav2353@gmail.com>
2022-01-11 23:54:47 +0530

    Clean up api urls

--- core/apps/api/urls.py
+++ core/apps/api/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from .views import RenewSubscription, AppStatusUpdate, AppConfirmationView, CustomerDataView, PingView, CustomerAppView
+from .views import CustomerDataView, PingView, CustomerAppView
 
 urlpatterns = [
-
-    path('subscription/renew/', RenewSubscription.as_view(), name='home'),
-    path('app/status/<str:app_id>/', AppStatusUpdate.as_view(), name='status'),
-    path('subscription/refund/', AppConfirmationView.as_view(), name='refund'),
     path('customer/<int:c_id>/apps/', CustomerAppView.as_view(), name="customer_apps"),
     path('customer/<int:c_id>/', CustomerDataView.as_view(), name="customer_data"),
     path('customer/', CustomerDataView.as_view(), name="customer"),
