2ce3e3c9c04e533cca1aa5301d34d6e27b24d0c3
ryuga <vaishnav2353@gmail.com>
2022-01-12 00:39:52 +0530

    Major code cleanup

--- core/apps/site/views.py
+++ core/apps/site/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.conf import settings
+from utils.oauth import Oauth
+
+oauth = Oauth(redirect_uri=settings.OAUTH_REDIRECT_URI, scope="identify%20email")
 
 
 class IndexView(View):
     template_name = "site/index.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, {"oauth_uri": oauth.discord_login_url})
 
 
